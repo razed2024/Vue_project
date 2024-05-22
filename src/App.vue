f9d112391583400b1f73fbaf25586cf6bac27557
shamim <razed3322@gmail.com>
2024-05-22 14:12:03 +0600

    First Component Completed

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from './components/HelloWorld.vue';
+import FirstComponent from './components/FirstComponent.vue';
 </script>
 
 <template>
     
     <div class="wrapper">
       <HelloWorld  />
+      <FirstComponent/>
     </div>
  
 
