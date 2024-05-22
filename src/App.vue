3ac4a7dee57ba87f1f3b49dca76af509a6fda1f3
shamim <razed3322@gmail.com>
2024-05-22 14:03:29 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+    
+    <div class="wrapper">
+      <HelloWorld  />
+    </div>
+ 
+
+  
+</template>
+
+<style scoped>
+
+</style>
